a2017df65caa5ff88c88d103cbe20227fb3cfcca
Ashwin-arul2112 <ashwinarul2112@gmail.com>
2026-02-13 21:29:51 +0530

    V8

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import UnityAR from "./UnityAR";
 
 function App() {
-  return <UnityAR />;
+
+  const startAR = async () => {
+
+    if (!navigator.xr) return alert("WebXR not supported");
+
+    const supported = await navigator.xr.isSessionSupported("immersive-ar");
+
+    if (!supported) return alert("AR not supported");
+
+    await navigator.xr.requestSession("immersive-ar", {
+      requiredFeatures: ["local-floor", "hit-test"]
+    });
+  };
+
+  return (
+    <div style={{ width:"100vw", height:"100vh", position:"relative" }}>
+
+      <button
+        onClick={startAR}
+        style={{ position:"absolute", zIndex:10, top:20, left:20 }}
+      >
+        Start AR
+      </button>
+
+      <UnityAR />
+
+    </div>
+  );
 }
 
 export default App;
--- src/UnityAR.jsx
+++ src/UnityAR.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function UnityAR() {
@@ -11,10 +10,8 @@ export default function UnityAR() {
   });
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Unity unityProvider={unityProvider}
-        style={{ width: "100%", height: "100%" }}
-      />
-    </div>
+    <Unity unityProvider={unityProvider}
+      style={{ width: "100%", height: "100%" }}
+    />
   );
 }
