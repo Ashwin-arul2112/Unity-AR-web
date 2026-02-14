cd5ddc581b54cfab9ec8e30163357757df607cec
Ashwin-arul2112 <ashwinarul2112@gmail.com>
2026-02-14 16:07:44 +0530

    V13

--- src/UnityAR.jsx
+++ src/UnityAR.jsx
@@ -3,10 +3,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 export default function UnityAR() {
 
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/unity/Build/WebGL_Builds.loader.js",
-    dataUrl: "/unity/Build/WebGL_Builds.data",
-    frameworkUrl: "/unity/Build/WebGL_Builds.framework.js",
-    codeUrl: "/unity/Build/WebGL_Builds.wasm",
+    loaderUrl: "/unity/Build/WebGL_Builds_Build_In.loader.js",
+    dataUrl: "/unity/Build/WebGL_Builds_Build_In.data",
+    frameworkUrl: "/unity/Build/WebGL_Builds_Build_In.framework.js",
+    codeUrl: "/unity/Build/WebGL_Builds_Build_In.wasm",
   });
 
   return (
